14ca679068c462d1a6cffd584e1508db1e619643
zxj <zxjViva@gmail.com>
2020-06-10 21:08:20 +0800

    改用moshi 代替gson

--- base/src/main/java/com/zxj/base/net/Net.kt
+++ base/src/main/java/com/zxj/base/net/Net.kt
@@ -1,16 +1,23 @@
 package com.zxj.base.net
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object Net {
     private val retrofit: Retrofit by lazy {
-        Retrofit.Builder().baseUrl("https://www.fastmock.site/mock/20e9f42cdade7baaf240e9f6de1e952e/wukong")
-            .addConverterFactory(GsonConverterFactory.create())
+        Retrofit.Builder()
+            .baseUrl("https://www.fastmock.site/mock/20e9f42cdade7baaf240e9f6de1e952e/wukong")
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                )
+            )
             .build()
     }
 
-    fun<T> create(service:Class<T> ): T {
+    fun <T> create(service: Class<T>): T {
         return retrofit.create(service)
     }
 }
