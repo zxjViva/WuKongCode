6df02285b3d6907a54951a73478b5cd1ae04c382
zxj <zxjViva@gmail.com>
2020-06-20 14:13:15 +0800

    购物车动画 体验, 需要把复制view 加到上层layout 中,否则在动画移动的时候会出现遮挡

--- app/src/main/java/com/zxj/wukong/adapter/CasesAdapter.kt
+++ app/src/main/java/com/zxj/wukong/adapter/CasesAdapter.kt
@@ -2,7 +2,7 @@ package com.zxj.wukong.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.zxj.wukong.BR
@@ -11,10 +11,16 @@ import com.zxj.wukong.data.CaseInfo
 import com.zxj.wukong.databinding.ItemCaseBinding
 
 class CasesAdapter(var data: List<CaseInfo>?) : RecyclerView.Adapter<CasesAdapter.Holder>() {
-    class Holder(var itemCaseBinding: ItemCaseBinding) : RecyclerView.ViewHolder(itemCaseBinding.root)
+    interface OnClickListener{
+        fun onClick(view: ImageView,caseInfo: CaseInfo)
+    }
+    var addBtClickListener:OnClickListener? = null
+
+    class Holder(var itemCaseBinding: ItemCaseBinding) :
+        RecyclerView.ViewHolder(itemCaseBinding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        return Holder(ItemCaseBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return Holder(ItemCaseBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -22,17 +28,19 @@ class CasesAdapter(var data: List<CaseInfo>?) : RecyclerView.Adapter<CasesAdapte
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.itemCaseBinding.setVariable(BR.caseInfo,data!![position])
+        val caseInfo = data!![position]
+        holder.itemCaseBinding.setVariable(BR.caseInfo, caseInfo)
         holder.itemCaseBinding.addView.setOnClickListener{
-            Toast.makeText(holder.itemView.context, "click add", Toast.LENGTH_SHORT).show()
+            addBtClickListener?.onClick(holder.itemCaseBinding.addView, caseInfo)
         }
 
-        holder.itemView.setOnClickListener{
-it.findNavController().navigate(R.id.action_homeFragment_to_caseDescFragment)
+        holder.itemView.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_caseDescFragment)
         }
     }
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.item_case
     }
+
 }
--- app/src/main/java/com/zxj/wukong/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/zxj/wukong/ui/fragments/HomeFragment.kt
@@ -1,24 +1,27 @@
 package com.zxj.wukong.ui.fragments
 
+import android.graphics.Path
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.github.florent37.viewanimator.ViewAnimator
 import com.zxj.base.net.NetResult
+import com.zxj.wukong.R
 import com.zxj.wukong.adapter.CasesAdapter
 import com.zxj.wukong.data.CaseInfo
 import com.zxj.wukong.databinding.FragmentHomeBinding
 import com.zxj.wukong.databinding.HomeHeaderLayoutBinding
 import com.zxj.wukong.viewmodels.CasesInfoViewModel
-import kotlin.concurrent.thread
 
 class HomeFragment : Fragment() {
-    lateinit var adapter: CasesAdapter
-    lateinit var bindView: FragmentHomeBinding
-    lateinit var headerBinding: HomeHeaderLayoutBinding
+    private lateinit var adapter: CasesAdapter
+    private lateinit var bindView: FragmentHomeBinding
+    private lateinit var headerBinding: HomeHeaderLayoutBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,20 +38,54 @@ class HomeFragment : Fragment() {
         model.casesInfo.observe(viewLifecycleOwner,
             Observer<NetResult<List<CaseInfo>>> {
                 if (it.result != null) {
-                    adapter = CasesAdapter(it.result)
-                    bindView.rv.adapter = adapter
+                    initAdapter(it.result!!)
                 }
-
             })
-        thread {
-            while (true) {
-                Thread.sleep(2000)
-                headerBinding.userTv.post {
-                    headerBinding.userTv.text = "sadasd"
-                }
+    }
+
+    private fun initAdapter(result: List<CaseInfo>) {
+        adapter = CasesAdapter(result)
+        bindView.rv.adapter = adapter
+        adapter.addBtClickListener = object :CasesAdapter.OnClickListener{
+            override fun onClick(view: ImageView, caseInfo: CaseInfo) {
+                shoppingCarAnimation(view)
             }
         }
-
+    }
+    //购物车动画
+    private fun shoppingCarAnimation(view: ImageView) {
+        val copyImageView = ImageView(view.context)
+        copyImageView.apply {
+            layoutParams = view.layoutParams
+            setImageResource(R.mipmap.icon_add)
+        }
+        //需要把复制的view 放到上层布局中,否则在动画执行的时候会出现遮挡
+        val viewGroup = view.rootView as ViewGroup
+        viewGroup.addView(copyImageView, ViewGroup.LayoutParams(view.width, view.height))
+        val startLocation = IntArray(2)
+        view.getLocationOnScreen(startLocation)
+        val stopLocation = IntArray(2)
+        bindView.shopcarIv.getLocationOnScreen(stopLocation)
+        copyImageView.x = startLocation[0].toFloat()
+        copyImageView.y = startLocation[1].toFloat()
+        val path = Path()
+        path.moveTo(startLocation[0].toFloat(), (startLocation[1]).toFloat())
+        path.quadTo(
+            (startLocation[0].toFloat() - viewGroup.width / 2),
+            startLocation[1].toFloat() - 300,
+            (stopLocation[0] + bindView.shopcarIv.width / 2).toFloat(),
+            (stopLocation[1]).toFloat()
+        )
+        ViewAnimator.animate(copyImageView)
+            .path(path)
+            .scale(0.5f)
+            .rotation(-360f)
+            .accelerate()
+            .duration(1000)
+            .onStop {
+                viewGroup.removeView(copyImageView)
+            }
+            .start()
     }
 
 }
